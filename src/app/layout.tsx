56991c84d275d70e9f0d226cd32d8ff8ff6c5158
Neel Raval <neel97531@gmail.com>
2025-07-12 07:39:38 +0000

    undeo last change quick

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import './globals.css';
 import { Toaster } from "@/components/ui/toaster"
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
-import DashboardLayout from './dashboard/layout';
 
 
 export const metadata: Metadata = {
