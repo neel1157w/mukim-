b50674884a0cc7562d99019b6c3ecbf65965fe18
Neel Raval <neel97531@gmail.com>
2025-07-12 08:06:43 +0000

    that footer take italic & bold

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,6 @@
 "use client";
 
-import Link from "next/link";
 import { usePathname } from 'next/navigation';
-import { Shirt } from "lucide-react";
 import { useEffect, useState } from "react";
 
 export default function Footer() {
@@ -27,7 +25,7 @@ export default function Footer() {
     <footer className="bg-background">
       <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-6">
         <div className="text-center text-sm text-sidebar-foreground">
-          <p>Made with ❤️, ~FourLoopers Team.</p>
+          <p className="font-bold italic">Made with ❤️, ~FourLoopers Team.</p>
         </div>
       </div>
     </footer>
